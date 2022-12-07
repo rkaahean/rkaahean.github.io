b721f01f935c50f9bbc443e2ef499bda314e7f81
Ranjan Srinivas <ranjan31051997@gmail.com>
2022-12-07 08:58:02 +0400

    more details

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
   return (
     <Container>
       <Text fz={50} fw={800}>
-        Welcome to my website!
+        Welcome!
       </Text>
       <Text
         sx={{
@@ -18,6 +18,10 @@ export const Home = () => {
           <Text span c="blue" inherit fw={500}>
             Ranjan Srinivas.
           </Text>
+        </Text>
+        <Text fz={20} fw={100}>
+          I work as a Machine Learning Engineer in Chippercash, attempting to
+          detect and prevent fraudulent actors in the P2P payments space.
         </Text>
       </Text>
 
