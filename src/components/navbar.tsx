0df159ded731d5fd5d22d6b605fefde97a2c606c
ranjan <ranjan@data-sleek.com>
2023-04-10 21:13:09 -0700

    site updates

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,11 @@ import Link from "next/link";
 export const Navbar = () => {
   return (
     <div className="flex h-16 w-full flex-row text-base text-stone-300">
-      <ul className={classNames("flex h-full flex-row items-center space-x-4")}>
+      <ul
+        className={classNames(
+          "flex h-full flex-row items-center space-x-4 font-semibold"
+        )}
+      >
         <li className="hover:text-stone-100">
           <Link href="/">Home</Link>
         </li>
@@ -14,9 +18,9 @@ export const Navbar = () => {
         </li>
       </ul>
       <ul className={classNames("ml-auto flex h-full items-center space-x-8")}>
-        <li className="hover:text-stone-100">
+        <li>
           <Link href="https://www.twitter.com/@rkaahean" target="_blank">
-            <div className="relative h-6 w-6">
+            <div className="relative h-6 w-6 filter-none hover:brightness-150">
               <Image
                 src={
                   "https://api.iconify.design/tabler:brand-twitter-filled.svg?color=%23888888"
@@ -27,12 +31,12 @@ export const Navbar = () => {
             </div>
           </Link>
         </li>
-        <li className="hover:text-stone-100">
+        <li>
           <Link
             href="https://www.linkedin.com/in/ranjansrinivas/"
             target="_blank"
           >
-            <div className="relative h-6 w-6">
+            <div className="relative h-6 w-6 hover:brightness-150">
               <Image
                 src={
                   "https://api.iconify.design/radix-icons:linkedin-logo.svg?color=%23888888"
@@ -43,9 +47,9 @@ export const Navbar = () => {
             </div>
           </Link>
         </li>
-        <li className="hover:text-stone-100">
+        <li>
           <Link href="https://www.github.com/rkaahean" target="_blank">
-            <div className="relative h-6 w-6">
+            <div className="relative h-6 w-6 hover:brightness-150">
               <Image
                 src={
                   "https://api.iconify.design/radix-icons:github-logo.svg?color=%23888888"
